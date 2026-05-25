adbe67e23919d65a6224ae5083cc2e31b70850d8
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 11:27:39 +0800

    Add Subnautica 2 (深海迷航2) guide

---
+++ app/subnautica2/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '深海迷航2攻略 / Subnautica 2 Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'survival': '生存与制造 / Survival & Crafting',
+  'biomes': '生态区与探索 / Biomes & Exploration',
+  'creatures': '生物图鉴 / Creatures Guide',
+  'story': '剧情与流程 / Story & Progression',
+}
