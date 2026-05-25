c8b60cff630c2ea4db348d6b1ddf1b690d35ccac
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 16:50:54 +0800

    add Dragon's Dogma 2 guide with 5 sections

---
+++ app/dragonsdogma2/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '龙之信条2攻略 / Dragon\'s Dogma 2 Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'world': '世界探索 / World Exploration',
+  'builds': '职业与Build / Vocations & Builds',
+  'bosses': 'BOSS攻略 / Boss Strategies',
+  'items': '装备物品 / Items & Gear',
+}
