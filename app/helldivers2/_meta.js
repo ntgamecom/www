419fa736c7916fb76841b42f71da6388bcfef4c6
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 15:39:50 +0800

    expand helldivers 2 guide: add weapons, armor, advanced tactics sections

---
+++ app/helldivers2/_meta.js
@@ -0,0 +1,16 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '绝地潜兵2攻略 / Helldivers 2 Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'stratagems': '战略配备与装备 / Stratagems & Gear',
+  'weapons': '武器装备 / Weapons & Equipment',
+  'armor': '护甲与增益 / Armor & Boosters',
+  'enemies': '敌人与派系 / Enemies & Factions',
+  'missions': '任务与成长 / Missions & Progression',
+  'advanced': '进阶技巧 / Advanced Tactics',
+}
