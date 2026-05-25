8507e8162eb3cefc60f523a0f9197799ae079704
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 19:19:36 +0800

    add Yakuza 8 guide (如龙8)

---
+++ app/yakuza8/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '如龙8攻略 / Like a Dragon: Infinite Wealth Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'jobs': '职业与技能 / Jobs & Skills',
+  'world': '世界与探索 / World & Exploration',
+  'quests': '故事与支线 / Story & Substories',
+  'items': '物品与收集 / Items & Collectibles',
+}
