0e77ac7d9533a3c6a2e6e0ca7dc2f3371688debf
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 16:15:41 +0800

    add Sekiro guide with 5 sections

---
+++ app/sekiro/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '只狼攻略 / Sekiro Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'world': '世界探索 / World Exploration',
+  'bosses': 'BOSS攻略 / Boss Strategies',
+  'prosthetics': '忍义手与技能 / Prosthetics & Skills',
+  'items': '物品收集 / Items & Collectibles',
+}
