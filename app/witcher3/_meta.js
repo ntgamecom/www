58b1676eda32a1e5618b5f5fe973af45c7316cd2
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 16:41:04 +0800

    add The Witcher 3 guide with 5 sections

---
+++ app/witcher3/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '巫师3攻略 / The Witcher 3 Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'world': '世界探索 / World Exploration',
+  'builds': '流派构建 / Build Guides',
+  'bosses': 'BOSS攻略 / Boss Strategies',
+  'items': '武器装备 / Items & Gear',
+}
