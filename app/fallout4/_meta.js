a7f05bd7d5f945048987bb773ad076d9361fca66
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 17:50:50 +0800

    add Fallout 4 guide (辐射4)

---
+++ app/fallout4/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '辐射4攻略 / Fallout 4 Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'world': '世界与探索 / World & Exploration',
+  'builds': 'Perk与Build / Perks & Builds',
+  'quests': '任务与阵营 / Quests & Factions',
+  'items': '物品与制造 / Items & Crafting',
+}
