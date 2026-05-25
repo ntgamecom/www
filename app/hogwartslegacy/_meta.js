054ca048043ebe9ba0a2f3e3c1d1eb9447507b5e
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 17:55:49 +0800

    add Hogwarts Legacy guide (霍格沃茨之遗)

---
+++ app/hogwartslegacy/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '霍格沃茨之遗攻略 / Hogwarts Legacy Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'world': '世界与探索 / World & Exploration',
+  'spells': '咒语与战斗 / Spells & Combat',
+  'quests': '任务与关系 / Quests & Relationships',
+  'items': '收集与装备 / Items & Collectibles',
+}
