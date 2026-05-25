b7bc3ad2e083e7dd30ff2317e5a09ea7c4950ada
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 19:13:28 +0800

    add Hades 2 guide (哈迪斯2)

---
+++ app/hades2/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '哈迪斯2攻略 / Hades II Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'boons': '恩赐与神明 / Boons & Gods',
+  'weapons': '武器与Build / Weapons & Builds',
+  'enemies': '敌人与Boss / Enemies & Bosses',
+  'items': '物品与升级 / Items & Upgrades',
+}
