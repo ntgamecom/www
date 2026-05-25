a5123b5877c7af92a92795c0e6a2cff5450826b1
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 17:24:01 +0800

    add GTA V guide (侠盗猎车手5)

---
+++ app/gta5/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '侠盗猎车手5攻略 / Grand Theft Auto V Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'story': '故事与任务 / Story & Missions',
+  'world': '世界与探索 / World & Exploration',
+  'online': '线上模式 / GTA Online',
+  'items': '载具与武器 / Vehicles & Weapons',
+}
