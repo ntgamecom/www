177aafea86b4fcbf8db46059d405ad642c731b46
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 15:18:57 +0800

    Add Red Dead Redemption 2 (荒野大镖客2) guide

---
+++ app/rdr2/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '荒野大镖客2攻略 / Red Dead Redemption 2 Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'world': '世界与探索 / World & Exploration',
+  'story': '剧情与任务 / Story & Missions',
+  'hunting': '狩猎与制作 / Hunting & Crafting',
+  'online': '线上模式 / Red Dead Online',
+}
