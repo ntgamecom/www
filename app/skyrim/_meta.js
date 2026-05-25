58946864a20be8893061f579de7ceeb1e557f877
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 17:30:41 +0800

    add Skyrim guide (上古卷轴5)

---
+++ app/skyrim/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '上古卷轴5攻略 / The Elder Scrolls V: Skyrim Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'world': '世界与探索 / World & Exploration',
+  'skills': '种族与技能 / Races & Skills',
+  'quests': '任务与阵营 / Quests & Factions',
+  'items': '装备与物品 / Items & Equipment',
+}
