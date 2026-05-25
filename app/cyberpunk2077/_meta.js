ae7abe08288bc79c3bc56e9f0afb209c8408d06d
ntgamecom <ntgamecom@users.noreply.github.com>
2026-05-25 16:33:02 +0800

    add Cyberpunk 2077 guide with 5 sections

---
+++ app/cyberpunk2077/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'full',
+      toc: false,
+    },
+  },
+  index: '赛博朋克2077攻略 / Cyberpunk 2077 Guide',
+  'beginner': '新手入门 / Beginner Guide',
+  'world': '夜之城探索 / World Exploration',
+  'builds': '流派构建 / Build Guides',
+  'bosses': 'BOSS攻略 / Boss Strategies',
+  'items': '武器装备 / Items & Gear',
+}
